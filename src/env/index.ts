449a4ac19e9ceebd3c14b7e5338415b2e04fa8ab
CleversonCSousa <cleversonsousa.git@gmail.com>
2026-03-26 12:03:06 -0300

    feat: setup basic infra with docker, prisma and zod env validation

---
+++ src/env/index.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_NAME: z.string(),
+  REDIS_PASSWORD: z.string(),
+  REDIS_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("Invalid environment variables", _env.error.format());
+
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
