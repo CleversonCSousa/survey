cbbaf50a62c90922d6614c01d5e8c84ad3347731
CleversonCSousa <cleversonsousa.git@gmail.com>
2026-04-04 14:14:49 -0300

    feat: setup swagger ui and document auth routes

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,31 @@ import fastifyJwt from "@fastify/jwt";
 import fastifyCookie from "@fastify/cookie";
 import { surveysRoutes } from "./http/routes/surveys-routes.ts";
 import { errorHandler } from "./http/errors/error-handler.ts";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 export const app = fastify();
+
+// Setup swagger with zod
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: { title: "Survey API", version: "1.0.0" },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
 app.setErrorHandler(errorHandler);
 app.register(fastifyCookie);
 app.register(fastifyJwt, {
@@ -17,5 +40,3 @@ app.register(usersRoutes);
 app.register(surveysRoutes, {
   prefix: "surveys",
 });
-
-// Error handler
--- src/http/routes/users-routes.ts
+++ src/http/routes/users-routes.ts
@@ -1,8 +1,56 @@
-import { FastifyInstance } from "fastify";
-import { register } from "@/http/controllers/register.ts";
+import { z } from "zod";
 import { authenticate } from "../controllers/authenticate.ts";
+import { FastifyInstance } from "fastify";
+import { registerBodySchema } from "../schemas/register-schema.ts";
+import { authenticateBodySchema } from "../schemas/authenticate-schema.ts";
+import { register } from "../controllers/register.ts";
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.post("/register", register);
-  app.post("/auth", authenticate);
+  app.post(
+    "/register",
+    {
+      schema: {
+        tags: ["Auth"],
+        summary: "Registrar novo Usuário",
+        description:
+          "Cria uma nova conta de usuário no sistema com a role RESPONDENT.",
+        body: registerBodySchema,
+        response: {
+          201: z.null().describe("Usuário criado com sucesso"),
+          409: z
+            .object({ message: z.string() })
+            .describe("Conflito: E-mail já cadastrado"),
+          500: z.null().describe("Erro interno no servidor"),
+        },
+      },
+    },
+    register,
+  );
+
+  app.post(
+    "/auth",
+    {
+      schema: {
+        tags: ["Auth"],
+        summary: "Autenticar um Usuário",
+        description:
+          "Autentica o usuário e retorna um Token JWT para acesso e define um Refresh Token via Cookie.",
+        body: authenticateBodySchema,
+        response: {
+          200: z
+            .object({
+              token: z.string(),
+            })
+            .describe("Autenticado com sucesso. Token JWT retornado."),
+          400: z
+            .object({
+              message: z.string(),
+            })
+            .describe("Credenciais inválidas (E-mail ou senha incorretos)"),
+          500: z.null().describe("Erro interno no servidor"),
+        },
+      },
+    },
+    authenticate,
+  );
 }
